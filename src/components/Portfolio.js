5d411a1c1bd3af6b80778be7c165af683414f8d6
Brandon <brandonbgrayson@gmail.com>
2021-02-23 12:31:05 -0500

    anime vs ufc image added with about the app text

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,6 +2,7 @@ import React from "react"
 import "../styles/portfolio.css"
 import lookbook from "../img/lookbook.png"
 import dmm from "../img/dmm.png"
+import anime from "../img/animevsmma.png"
 
 
 const Portfolio = () => {
@@ -50,13 +51,12 @@ const Portfolio = () => {
                     <div className="card mb-3" style={{ "maxWidth": "540px" }}>
                         <div className="row g-0">
                             <div className="col-md-4">
-                                <img src="..." alt="..." />
+                                <img src={anime} alt="anime vs ufc homepage" style={{"maxWidth": "200px"}} />
                             </div>
                             <div className="col-md-8">
                                 <div className="card-body">
                                     <h5 className="card-title">Anime Vs UFC</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                                    <p className="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+                                    <p className="card-text">Allows users to watch and vote on their favorite action scenes from anime classics and UFC live events'</p>
                                 </div>
                             </div>
                         </div>
