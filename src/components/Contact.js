4315b598db04a07cbbb2dced7dccf48a1156d602
Brandon <brandonbgrayson@gmail.com>
2021-02-24 21:48:08 -0500

    added resume img to contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,13 @@
 import React from "react"
-import img from "../"
+import "../styles/contact.css"
+import resume from "../img/resume.png"
 
 const Contact = () => {
     return (
         <div className=' container jumbotron col-xs-12 col-sm-12 col-md-12 col-lg-12'>
             <h1 className='txt'>Lets Make Something Great</h1>
 
-            
+            <img className='resume' src={resume} />
 
 
         </div>
