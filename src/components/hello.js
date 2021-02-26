bca82cd0c30a66e142cee2cff259bfc9d3ddea56
Brandon <brandonbgrayson@gmail.com>
2021-02-26 17:21:19 -0500

    added Link to homepage from Navbar component in about.js

--- src/components/hello.js
+++ src/components/hello.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import img from "../img/Brandon_cropped.jpg"
+import { Link } from "react-router-dom";
+import img from "../img/Brandon_cropped.jpg";
 
 
 
