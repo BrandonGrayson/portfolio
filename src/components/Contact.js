796493270e0f545464ca88e3d573ca459ffc9582
Brandon <brandonbgrayson@gmail.com>
2021-02-26 17:26:21 -0500

    added Navbar component to Contact and Portfolio components

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,6 @@
 import React from "react"
 import "../styles/contact.css"
+import Navbar from "./Navbar"
 import resume from "../img/resume.png"
 import resumePDF from "../img/resumepdf.pdf"
 
@@ -7,6 +8,8 @@ const Contact = () => {
     return (
         <div className=' container jumbotron mb-0 col-xs-12 col-sm-12 col-md-12 col-lg-12'>
             <h1 className='txt'>Lets Make Something Great</h1>
+
+            <Navbar />
 
             <div>
                 <a href={resumePDF}>Download Resume</a>
